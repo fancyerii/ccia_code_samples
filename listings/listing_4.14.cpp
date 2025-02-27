f22b5be16909d10077ac9f809f52e793af32a2df
李理 <fancyerii@gmail.com>
2025-02-27 10:42:28 +0800

    Update listing_4.14.cpp
    fix compile error.

--- listings/listing_4.14.cpp
+++ listings/listing_4.14.cpp
@@ -1,9 +1,10 @@
 #include <future>
+#include <type_traits>
 template<typename F,typename A>
-std::future<std::result_of<F(A&&)>::type>
+typename std::future<std::result_of<F(A&&)>>::type
 spawn_task(F&& f,A&& a)
 {
-    typedef std::result_of<F(A&&)>::type result_type;
+    typedef typename std::result_of<F(A&&)>::type result_type;
     std::packaged_task<result_type(A&&)>
         task(std::move(f));
     std::future<result_type> res(task.get_future());
